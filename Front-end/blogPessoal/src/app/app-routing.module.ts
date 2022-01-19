44304af6583096c83792c0d52cf867178afdce65
Guilherme Moreira <guilhermemoreira111@hotmail.com>
2022-01-19 12:51:46 -0300

    Implementando Rotas no Angular

--- Front-end/blogPessoal/src/app/app-routing.module.ts
+++ Front-end/blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+
+  {path:'menu', component: MenuComponent},
+  {path:'entrar', component: EntrarComponent},
+  {path:'cadastro', component: CadastroComponent},
+  {path:'rodape', component: RodapeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-end/blogPessoal/src/app/app.module.ts
+++ Front-end/blogPessoal/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { RodapeComponent } from './rodape/rodape.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    CadastroComponent,
+    EntrarComponent,
     MenuComponent,
     RodapeComponent,
-    CadastroComponent,
-    EntrarComponent
+
   ],
   imports: [
     BrowserModule,
